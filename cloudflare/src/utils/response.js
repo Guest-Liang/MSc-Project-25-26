7c7fe92559f41d801e75ea1d04ccb9d68640dc12
GuestLiang <GuestLiang@outlook.com>
2025-11-25 15:17:10 +0000

    :sparkles: Update wrangler.toml, update response json format

--- cloudflare/src/utils/response.js
+++ cloudflare/src/utils/response.js
@@ -1,6 +1,17 @@
-export function jsonResponse(obj, status = 200) {
-  return new Response(JSON.stringify(obj), {
+export function jsonResponse(data, error = null, status = 200) {
+  const body = {
+    success: !error,
+    data: error ? null : data,
+    error: error
+      ? {
+          code: error.code || "UNKNOWN_ERROR",
+          message: error.message || error
+        }
+      : null
+  }
+
+  return new Response(JSON.stringify(body), {
     status,
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json" }
   })
 }
