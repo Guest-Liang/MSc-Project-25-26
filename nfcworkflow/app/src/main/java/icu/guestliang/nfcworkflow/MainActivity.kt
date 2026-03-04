61bd30cbd2d41e9e69b45eff2ea4b414cdf19b56
GuestLiang <GuestLiang@outlook.com>
2026-03-04 02:03:17 +0000

    :recycle: Huge UI architecture refactoring

--- nfcworkflow/app/src/main/java/icu/guestliang/nfcworkflow/MainActivity.kt
+++ nfcworkflow/app/src/main/java/icu/guestliang/nfcworkflow/MainActivity.kt
@@ -2,27 +2,39 @@ package icu.guestliang.nfcworkflow
 
 import icu.guestliang.nfcworkflow.data.PrefsDataStore
 import icu.guestliang.nfcworkflow.logging.AppLogger
+import icu.guestliang.nfcworkflow.navigation.NavGraph
 import icu.guestliang.nfcworkflow.ui.theme.NFCWorkFlowTheme
-import icu.guestliang.nfcworkflow.ui.view.RootScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.core.view.WindowCompat
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppLogger.info(this, "MainActivity created", "App")
 
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        // Use the modern EdgeToEdge API
+        enableEdgeToEdge()
 
         setContent {
             val prefs by PrefsDataStore.flow(this).collectAsState(initial = null)
             prefs?.let { currentPrefs ->
                 NFCWorkFlowTheme(prefs = currentPrefs) {
-                    RootScreen()
+                    val navController = rememberNavController()
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        NavGraph(
+                            navController = navController,
+                            modifier = Modifier.fillMaxSize().padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
